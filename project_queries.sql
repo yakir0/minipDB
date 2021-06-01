170d3bbc604c4333d7176f61e3fc564062dad19c
Segev608 <57367786+Segev608@users.noreply.github.com>
2021-06-01 16:18:11 +0300

    Update project_queries.sql

--- project_queries.sql
+++ project_queries.sql
@@ -1,25 +1,36 @@
 -- Query 1 : all pending requests [TRIVIAL]
-select requ_id, requester_id, reciever_id
+select requ_id as Request_Num,
+       requester_id as Request_Id,
+       reciever_id as Reciever_Id
 from friendshiprequest
 where approved is null;
 -- Result: 6616 rows in 6.427 sec
 
 -- Query 2 : most active (more then X notifications) profiles [TRIVIAL]
-select profile_id, first_name, last_name, count(*) as notf_num
+select profile_id as Profile_Id,
+       first_name as First_Name,
+       last_name as Last_Name,
+       count(*) as Notification_Amount
 from notification N natural join afeder.profile P
 group by profile_id, first_name, last_name
 having count(*) > 5;
 -- Result: 166 rows in 6.535 sec
 
 -- Query 3 : most recommended profiles [TRIVIAL]
-select offerid, first_name, last_name, count(*) as rec_num
+select offerid as Offer_Id,
+       first_name as First_Name,
+       last_name as Last_Name,
+       count(*) as Request_Amount
 from friendshipsuggestion FS join afeder.profile P on offerid = profile_id
 group by offerid, first_name, last_name
 having count(*) > 5;
 -- Result: 156 rows in 6.663 sec
 
 -- Query 4 : most desprate profiles [TRIVIAL]
-select requester_id, first_name, last_name, count(*) as req_num
+select requester_id as Requester_Id,
+       first_name as First_Name,
+       last_name as Last_Name,
+       count(*) as Request_Amount
 from friendshiprequest FR join afeder.profile P on requester_id = profile_id
 group by requester_id, first_name, last_name
 having count(*) > 5;
@@ -58,14 +69,19 @@ where N.Watched = 1 and
 -- Result: 9807 rows in 12.549 sec
 
 -- Query 7 : all popular profiles - more then 5 requests
-select FR.RECIEVER_ID, first_name, last_name, count(*) as req_num
+select FR.RECIEVER_ID as FriendshipRequest_Id,
+       first_name as First_Name,
+       last_name as Last_Name,
+       count(*) as Request_Amount
 from friendshiprequest FR join afeder.profile P on FR.RECIEVER_ID = P.PROFILE_ID
 group by FR.RECIEVER_ID, first_name, last_name
 having count(*) > 5;
 -- Result: 176 rows in 12.624 sec
 
 ---- Query 8 : suspicious activity
-select P.profile_id, first_name, last_name
+select P.profile_id as Profile_Id,
+       first_name as First_Name,
+       last_name as Last_Name
 from afeder.profile P
 where P.GENDER = 'M' and
       (select count(*)
